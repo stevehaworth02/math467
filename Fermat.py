107e99aadee0cac51d1979c96a297de8f3c613c7
Steven Haworth <stevenhaworth@Stevens-MacBook-Pro.local>
2025-05-14 11:14:58 -0500

    Fermat primes 1-4

--- Fermat.py
+++ Fermat.py
@@ -1,19 +1,21 @@
 from sympy import isprime
 
-def mersenne_number(p):
-    return 2**p - 1
+def fermat_prime(n):
+    return 2**2**n + 1
 
 def main():
     try:
-        n = int(input("Enter how many Mersenne numbers to check (based on exponent p): "))
+        n = int(input("Enter how many Fermat numbers to check (based on exponent n): "))
     except ValueError:
         print("Please enter a valid integer.")
         return
 
-    for p in range(2, n + 2):  # +2 so it includes the nth exponent starting from 2
-        m = mersenne_number(p)
+    for p in range(n):  # +2 so it includes the nth exponent starting from 2
+        m = fermat_prime(p)
         prime_status = isprime(m)
         print(f"Number: {m} Is Prime: {str(prime_status).upper()}")
 
 if __name__ == "__main__":
     main()
+
+
